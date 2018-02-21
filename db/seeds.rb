c61e95739b7a392d3192c5092f793b632fea5f8b
Ben Fanning fanninggg@gmail.com <fanninggg@gmail.com>
2018-02-21 15:01:30 +0000

    Yelp finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+Restaurant.destroy_all
+
+10.times do
+  restaurants = Restaurant.new(
+    name: Faker::HitchhikersGuideToTheGalaxy.starship,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+    phone_number: Faker::Number.number(10),
+    address: Faker::HitchhikersGuideToTheGalaxy.location
+  )
+  restaurants.save!
+end
+
